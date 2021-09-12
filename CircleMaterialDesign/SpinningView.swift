0df6dae728a5cbe14e77a4fec45325bb0d8dcc54
Simon McNeil <simonmcneil062@gmail.com>
2021-09-12 17:43:43 -0400

    first commit

---
+++ CircleMaterialDesign/SpinningView.swift
@@ -0,0 +1,20 @@
+//
+//  SpinningView.swift
+//  CircleMaterialDesign
+//
+//  Created by Simon Mcneil on 2021-09-10.
+//
+
+import UIKit
+
+class SpinningView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
